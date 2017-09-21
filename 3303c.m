15b901a699960a8fd5a663eab3af80eac9e0086e
Paul <git@dm2pf.de>
2017-09-21 16:37:13 +0200

    initial commit, code added

---
+++ 3303c.m
@@ -0,0 +1,38 @@
+%% Instrument Configuration and Control
+%% Communicating with instrument object, obj1.
+
+% Turn Channels on and off
+fprintf(obj1, 'OUTPut CH1, ON');
+fprintf(obj1, 'OUTPut CH1, OFF');
+
+% Select the channel that will be operated
+fprintf(obj1, 'INSTRUMENT CH1');
+% Query the current operating channel
+fprintf(obj1, 'INSTRUMENT?');
+fscanf(obj1)
+
+% Select the Operation Mode,
+% 0: Independant, 1: Series, 2: Parallel
+fprintf(obj1, 'OUTPut:TRACK 1');
+
+% Set Voltage to 13.37 V
+fprintf(obj1, 'CH1:VOLTage 13.37');
+
+% Set Current limit to 2.5 A
+fprintf(obj1, 'CH2:CURRent 2.5');
+
+% Read Set Voltage
+fprintf(obj1, 'CH1:VOLTage?');
+U1 = str2double(fscanf(obj1))
+
+% Read Set Current
+fprintf(obj1, 'CH1:CURRent?');
+I1 = str2double(fscanf(obj1))
+
+% Measure actual Voltage
+fprintf(obj1, 'MEAsure:VOLTage? CH1');
+U1 = str2double(fscanf(obj1))
+
+% Measure actual Current
+fprintf(obj1, 'MEAsure:CURRent? CH1');
+I1 = str2double(fscanf(obj1))
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SPD3303C_Matlab
-Matlab Code to control the Siglent SPD3303C Lab Bench Power Supply
+Matlab Code to control the Siglent SPD3303C Lab Bench Power Supply.
+NI VISA Drivers might be necessary.
+Create an initialisation script with the Instrument Control Toolbox first.
